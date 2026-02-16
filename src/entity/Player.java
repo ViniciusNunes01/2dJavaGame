463b05d76609820667e73f544a39ee861bb8645a
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-15 21:46:40 -0300

    player ypdates

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -67,7 +67,7 @@ public class Player extends Entity {
 		}
 
 		spriteCounter++;
-		if (spriteCounter > 12) {
+		if (spriteCounter > 15) {
 			if (spriteNum == 1) {
 				spriteNum = 2;
 			} else if (spriteNum == 2) {
