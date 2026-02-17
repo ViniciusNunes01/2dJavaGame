d0daf5e63c3dd59d390c94b9aea2587f4ea99526
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-16 21:25:27 -0300

    player movement checked with collisions

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,7 +1,7 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -25,6 +25,13 @@ public class Player extends Entity {
 
 		screenX = gp.screenWidth / 2 - (gp.tileSize / 2); // personagem inicia no centro da tela
 		screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+
+		solidArea = new Rectangle(); // retângulo para hitbox do personagem (x, y, width, height)
+		solidArea.x = 8;
+		solidArea.y = 16; // o retângulo será localizado dentro do personagem, menor que o tamanho dele
+							// (48x48)
+		solidArea.width = 32;
+		solidArea.height = 32;
 
 		setDefaultValues();
 		getPlayerImage();
@@ -60,16 +67,38 @@ public class Player extends Entity {
 
 		if (keyH.upPressed == true) {
 			direction = "up";
-			worldY -= speed;
 		} else if (keyH.downPressed == true) {
 			direction = "down";
-			worldY += speed;
 		} else if (keyH.leftPressed == true) {
 			direction = "left";
-			worldX -= speed;
 		} else if (keyH.rightPressed == true) {
 			direction = "right";
-			worldX += speed;
+		}
+
+		// CHECK TILE COLLISION
+		collisionOn = false;
+		gp.cChecker.checkTile(this);
+
+		// IF COLLISION IS FALSE, PLAYER CAN MOVE
+		if (collisionOn == false) {
+			if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true
+					|| keyH.rightPressed == true) {
+				switch (direction) {
+				case "up":
+					worldY -= speed;
+					break;
+				case "down":
+					worldY += speed;
+					break;
+				case "left":
+					worldX -= speed;
+					break;
+				case "right":
+					worldX += speed;
+					break;
+				}
+			}
+
 		}
 
 		spriteCounter++;
