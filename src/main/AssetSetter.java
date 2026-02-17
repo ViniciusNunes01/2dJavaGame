0b71e2f8240213633a353302ee350be7af1909e0
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-17 18:33:24 -0300

    class to set assets on map

---
+++ src/main/AssetSetter.java
@@ -0,0 +1,41 @@
+package main;
+
+import object.OBJ_Chest;
+import object.OBJ_Door;
+import object.OBJ_Key;
+
+public class AssetSetter {
+
+	GamePanel gp;
+
+	public AssetSetter(GamePanel gp) {
+		this.gp = gp;
+	}
+
+	public void setObject() {
+
+		gp.obj[0] = new OBJ_Key();
+		gp.obj[0].worldX = 21 * gp.tileSize;
+		gp.obj[0].worldY = 16 * gp.tileSize;
+
+		gp.obj[1] = new OBJ_Key();
+		gp.obj[1].worldX = 44 * gp.tileSize;
+		gp.obj[1].worldY = 19 * gp.tileSize;
+		
+		gp.obj[2] = new OBJ_Key();
+		gp.obj[2].worldX = 11 * gp.tileSize;
+		gp.obj[2].worldY = 37 * gp.tileSize;
+		
+		gp.obj[3] = new OBJ_Door();
+		gp.obj[3].worldX = 3 * gp.tileSize;
+		gp.obj[3].worldY = 14 * gp.tileSize;
+		
+		gp.obj[4] = new OBJ_Door();
+		gp.obj[4].worldX = 4 * gp.tileSize;
+		gp.obj[4].worldY = 14 * gp.tileSize;
+		
+		gp.obj[5] = new OBJ_Chest();
+		gp.obj[5].worldX = 3 * gp.tileSize;
+		gp.obj[5].worldY = 3 * gp.tileSize;
+	}
+}
